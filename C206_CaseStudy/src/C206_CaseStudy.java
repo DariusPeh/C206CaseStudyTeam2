0771ca6465e9dbeec2dd6f6b5e962230aa31b4ed
mingyao1234 <21023135@myrp.edu.sg>
2022-08-08 15:23:33 +0800

    test

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -2,14 +2,7 @@
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
-		
-		Helper.line(30, "=");
-		System.out.println("CCA Registration system - LOGIN");
-		Helper.line(30, "=");
-		
-		String username = Helper.readString("Enter Student ID > ");
-		String ccaRegID = Helper.readString("Enter CCA registration ID > ");
-		String password = Helper.readString("Enter Password > ");
+		System.out.println("Hello World");
 	}
 
 }
