588477193f682aa89fd393c77bd3cc3c90a0cb32
DariusPeh <21015463@myrp.edu.sg>
2022-08-08 15:37:59 +0800

    Added Parents Class

---
+++ C206_CaseStudy/src/parents.java
@@ -0,0 +1,23 @@
+
+public class parents extends students{
+	
+	private String pName;
+	private String pEmail;
+	private int pContactNo;
+	
+	public parents(String pName, String pEmail, int pContactNo) {
+		this.pName = pName;
+		this.pEmail = pEmail;
+		this.pContactNo = pContactNo;
+	}
+	
+	public String getpName() {
+		return pName;
+	}
+	public String getpEmail() {
+		return pEmail;
+	}
+	public int getpContactNo() {
+		return pContactNo;
+	}
+}
