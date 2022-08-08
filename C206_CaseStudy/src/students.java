88de592e616bd824c555340786d3ed4f9f428962
auful <auful@DESKTOP-1TMV29S.mshome.net>
2022-08-08 20:15:55 +0800

    students class added

--- C206_CaseStudy/src/students.java
+++ C206_CaseStudy/src/students.java
@@ -1,8 +1,31 @@
+public class students{
+	
+	private String sName;
+	private String sEmail;
+	private int sContactNo;
+	private int sStudentId;
 
-public class students {
-
-	public students() {
-		// TODO Auto-generated constructor stub
+	public students(String sName, String sEmail, int sContactNo, int sStudentId) {
+		super();
+		this.sName = sName;
+		this.sEmail = sEmail;
+		this.sContactNo = sContactNo;
+		this.sStudentId = sStudentId;
 	}
-
+	
+	public String getsName() {
+		return sName;
+	}
+	public String getsEmail() {
+		return sEmail;
+	}
+	public int getsContactNo() {
+		return sContactNo;
+	}
+	public int getsStudentId() {
+		return sStudentId;
+	}
+	
+	
 }
+	
