c5c8c9f7db63082112fc8461105047a75de14615
20011490 <20011490@LAPTOP-3NC72DTL>
2022-08-11 16:36:11 +0800

    Added CCA class

---
+++ C206_CaseStudy/src/CCA.java
@@ -0,0 +1,97 @@
+/**
+ * I declare that this code was written by me on 2022-Aug-11 4:28:48 PM 
+ * @author 20011490
+ * 
+ */
+
+public class CCA {
+	
+	public String cTitle;
+	public String description;
+	public int classSize;
+	public String day;
+	public int time;
+	public String venue;
+	public boolean isAvailable;
+	public String cInstructor;
+
+	public CCA(String cTitle, String description, int classSize, String day, int time, String venue,
+			boolean isAvailable, String cInstructor) {
+		super();
+		this.cTitle = cTitle;
+		this.description = description;
+		this.classSize = classSize;
+		this.day = day;
+		this.time = time;
+		this.venue = venue;
+		this.isAvailable = isAvailable;
+		this.cInstructor = cInstructor;
+	}
+
+	public String getcTitle() {
+		return cTitle;
+	}
+
+	public void setcTitle(String cTitle) {
+		this.cTitle = cTitle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getClassSize() {
+		return classSize;
+	}
+
+	public void setClassSize(int classSize) {
+		this.classSize = classSize;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	public String getVenue() {
+		return venue;
+	}
+
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+
+	public String getcInstructor() {
+		return cInstructor;
+	}
+
+	public void setcInstructor(String cInstructor) {
+		this.cInstructor = cInstructor;
+	}
+	
+}
+
+}
