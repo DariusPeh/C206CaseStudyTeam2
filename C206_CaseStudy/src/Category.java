fe78d07b309dd670ff5f641a3885ba3e70ebd934
Hanizalyssa <20007201@myrp.edu.sg>
2022-08-12 21:21:08 +0800

    haniz category

---
+++ C206_CaseStudy/src/Category.java
@@ -0,0 +1,43 @@
+/**
+ * I declare that this code was written by me on 2022-Aug-11 4:28:48 PM 
+ * @author 20011490
+ * 
+ */
+
+public class Category extends CCA {
+	
+	private String ctName;
+	private int ctId;
+	private String ctDescription;
+
+	public Category(String ctName, int ctId, String ctDesription) {
+		super();
+		this.ctName = ctName;
+		this.ctId = ctId;
+		this.ctDescription = ctDescription;
+	}
+
+	public String getctName() {
+		return ctName;
+	}
+
+	public void setctName(String ctName) {
+		this.ctName = ctName;
+	}
+
+	public int getctId() {
+		return ctId;
+	}
+
+	public void setctId(int ctId) {
+		this.ctId = ctId;
+	}
+
+	public String getctDescription() {
+		return ctDescription;
+	}
+	
+	public void setctDescription(String ctDescription) {
+		this.ctDescription = ctDescription;
+	}
+	}
