2a4564e519f0d36f91768c696984963d9e0a6195
20011490 <20011490@LAPTOP-3NC72DTL>
2022-08-15 10:10:52 +0800

    darius retrieveparent

---
+++ C206_CaseStudy/test/retrieveParent.java
@@ -0,0 +1,14 @@
+/**
+ * I declare that this code was written by me on 2022-Aug-14 11:50:12 PM 
+ * @author 20011490
+ * 
+ */
+
+public class retrieveParent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
